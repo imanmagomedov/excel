28fe38c9e46626d3e166867ad8b879826127796d
Said Imanmagomedov <imanmagomedov.said@gmail.com>
2020-06-06 17:23:35 +0300

    finish base framework functional

--- src/core/dom.js
+++ src/core/dom.js
@@ -21,7 +21,13 @@ class Dom {
     return this
   }
 
-  on() {}
+  on(eventType, callback) {
+    this.$el.addEventListener(eventType, callback)
+  }
+
+  off(eventType, callback) {
+    this.$el.removeEventListener(eventType, callback)
+  }
 
   append(node) {
     if (node instanceof Dom) {
