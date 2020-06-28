8c5e1eeeabb3376adf777b6297abe243069429a1
Said Imanmagomedov <imanmagomedov.said@gmail.com>
2020-06-28 19:01:29 +0300

    add resize for row and col

--- src/components/table/Table.js
+++ src/components/table/Table.js
@@ -1,10 +1,30 @@
 import {ExcelComponent} from '@core/ExcelComponent'
 import {createTable} from '@/components/table/table.template'
+import {$} from '@core/dom'
 
 export class Table extends ExcelComponent {
   static className = 'excel__table'
 
+  constructor($root) {
+    super($root, {
+      listeners: ['mousedown']
+    })
+  }
+
   toHTML() {
     return createTable(30)
+  }
+
+  onMousedown(event) {
+    if (event.target.dataset.resize) {
+      const $resizer = $(event.target)
+      const $parent = $resizer.closest('[data-type="resizable"]')
+      const coords = $parent.getCoords()
+
+      document.onmousemove = e => {
+        const delta = e.pageX - coords.right
+        $parent.$el.style.width = (coords.width + delta) + 'px'
+      }
+    }
   }
 }
--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -4,16 +4,25 @@ const CODES = {
 }
 
 function createRow(index, content) {
+  const resize = index ? '<div class="row-resize" data-resize="row"></div>' : ''
   return `
     <div class="row">
-      <div class="row-info">${index ? index : ''}</div>
+      <div class="row-info" data-type="resizable">
+        ${index ? index : ''}
+        ${resize}
+      </div>
       <div class="row-data">${content}</div>
     </div>
   `
 }
 
 function toColumn(col) {
-  return `<div class="column">${col}</div>`
+  return `
+    <div class="column" data-type="resizable">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
+  `
 }
 
 function toCell() {
